d2b86e8103ea05185684e34251517b8d4b0c113a
DiegoSanchezC <diego270@outlook.com>
2024-09-08 19:15:40 -0500

    Mi primer commit

---
+++ src/main/java/com/techcommunityperu/techcommunityperu/TechcommunityperuApplication.java
@@ -0,0 +1,13 @@
+package com.techcommunityperu.techcommunityperu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcommunityperuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechcommunityperuApplication.class, args);
+    }
+
+}
