8354bff611a46e670ef7a1a8f565e425d9720cec
dsanchezchu <dsanchezc9@upao.edu.pe>
2024-09-18 16:27:20 -0500

    feat(backend): Registro de usuarios
    Se logro un correcto registro de usuarios y se hizo prueba con postman funcionando de igual manera, siguiendo lo indicado con el video :
    https://www.youtube.com/watch?v=kuteqlSb0LA

---
+++ src/main/java/com/techcommunityperu/techcommunityperu/api/AuthController.java
@@ -0,0 +1,24 @@
+package com.techcommunityperu.techcommunityperu.api;
+
+import com.techcommunityperu.techcommunityperu.model.entity.Usuario;
+import com.techcommunityperu.techcommunityperu.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Usuario> register(@RequestBody Usuario usuario) {
+        Usuario newUser= userService.registrarUsuario(usuario);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+}
