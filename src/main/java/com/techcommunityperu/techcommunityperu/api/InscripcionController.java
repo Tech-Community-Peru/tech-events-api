b27f0c9e1d78e3563911c655326516f415b54f32
Diego Sanchez Chuquimango <165108758+dsanchezchu@users.noreply.github.com>
2024-09-19 18:37:19 -0500

    Update InscripcionController.java

--- src/main/java/com/techcommunityperu/techcommunityperu/api/InscripcionController.java
+++ src/main/java/com/techcommunityperu/techcommunityperu/api/InscripcionController.java
@@ -1,13 +1,13 @@
 package com.techcommunityperu.techcommunityperu.api;
-
 import com.techcommunityperu.techcommunityperu.service.InscripcionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
-@RequestMapping("/inscripciones")
+@RequestMapping("/inscripcion")
 @RequiredArgsConstructor
 public class InscripcionController {
 
