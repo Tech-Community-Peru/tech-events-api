ec1be02b3df7b6dc6fddca52ba1c5931d88e0e93
Diego Sanchez Chuquimango <165108758+dsanchezchu@users.noreply.github.com>
2024-09-19 17:46:50 -0500

    Update UserRepository.java

--- src/main/java/com/techcommunityperu/techcommunityperu/repository/UserRepository.java
+++ src/main/java/com/techcommunityperu/techcommunityperu/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Usuario, Integer> {
     boolean existsByCorreoElectronico(String correoElectronico);
+    Optional<Usuario> findByCorreoElectronico(String correoElectronico);
 }
