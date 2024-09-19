dfcb75a8eea3f3dc255f1bc71168bc87d343c5f2
Diego Sanchez Chuquimango <165108758+dsanchezchu@users.noreply.github.com>
2024-09-19 18:33:30 -0500

    Update InscripcionService.java

--- src/main/java/com/techcommunityperu/techcommunityperu/service/InscripcionService.java
+++ src/main/java/com/techcommunityperu/techcommunityperu/service/InscripcionService.java
@@ -1,6 +1,6 @@
 package com.techcommunityperu.techcommunityperu.service;
-
 import com.techcommunityperu.techcommunityperu.model.entity.Inscripcion;
+import java.util.Optional;
 
 public interface InscripcionService {
     void cancelarInscripcion(Integer eventoId, Integer usuarioId);
