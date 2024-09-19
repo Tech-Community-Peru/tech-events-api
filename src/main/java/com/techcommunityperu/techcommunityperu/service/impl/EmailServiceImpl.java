a6c5fb3c40a7107c62234f4cd7d8a5ec18f0fc87
CharlieMoralesBenites <166151957+CharlieMoralesBenites@users.noreply.github.com>
2024-09-19 13:43:37 -0500

    feat(Backend)notificacion-pago
    complementación de emailservice

--- src/main/java/com/techcommunityperu/techcommunityperu/service/impl/EmailServiceImpl.java
+++ src/main/java/com/techcommunityperu/techcommunityperu/service/impl/EmailServiceImpl.java
@@ -13,5 +13,12 @@ public class EmailServiceImpl implements EmailService {
     public void sendConfirmationEmail(Inscripcion inscripcion, double monto) {
         // En una implementacion real, aquí iría la lógica de envío de correos electrónicos
         System.out.println("Enviando correo de confirmación para la inscripción ID: " + inscripcion.getId());
+        System.out.println("Resumen de compra: " +inscripcion.getInscripcionStatus()+"\n"
+                +"ID Evento: "+inscripcion.getEvento().getId()+"\n"
+                +"Nombre de Evento: "+inscripcion.getEvento().getNombre()+"\n"
+                +"ID Usuario :"+inscripcion.getUsuario().getId()+"\n"
+                +"Nombre de Usuario :"+inscripcion.getUsuario().getNombre()+"\n"
+                +"Tipo de pago: "+inscripcion.getTipoPago()+"\n"
+                +"Monto pagado: "+inscripcion.getMonto());
     }
 }
