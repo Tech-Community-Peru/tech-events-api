e26655c62945b595751b381563b82a72ca989dfa
DanielRumay <165932669+DanielRumay@users.noreply.github.com>
2024-10-15 23:21:19 -0500

    Api de Pay Pal part 3 - creacion de DTO, service-serviceimpl y controller

---
+++ src/main/java/com/techcommunityperu/techcommunityperu/service/impl/CheckoutServiceImpl.java
@@ -0,0 +1,51 @@
+package com.techcommunityperu.techcommunityperu.service.impl;
+
+import com.techcommunityperu.techcommunityperu.dto.PaymentCaptureResponse;
+import com.techcommunityperu.techcommunityperu.dto.PaymentOrderResponse;
+import com.techcommunityperu.techcommunityperu.integration.payment.paypal.dto.OrderCaptureResponse;
+import com.techcommunityperu.techcommunityperu.integration.payment.paypal.dto.OrderResponse;
+import com.techcommunityperu.techcommunityperu.integration.payment.paypal.service.PayPalService;
+import com.techcommunityperu.techcommunityperu.service.CheckoutService;
+import com.techcommunityperu.techcommunityperu.service.PurchaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CheckoutServiceImpl implements CheckoutService {
+
+    private final PayPalService payPalService;
+    private final PurchaseService purchaseService;
+
+    @Override
+    public PaymentOrderResponse createPaymentUrl(Integer purchaseId, String returnUrl, String cancelUrl) {
+        OrderResponse orderResponse = payPalService.createOrder(purchaseId, returnUrl, cancelUrl);
+
+        String paypalUrl = orderResponse
+                .getLinks()
+                .stream()
+                .filter(link -> link.getRel().equals("approve"))
+                .findFirst()
+                .orElseThrow(RuntimeException::new)
+                .getHref();
+
+        return new PaymentOrderResponse(paypalUrl);
+    }
+
+    @Override
+    public PaymentCaptureResponse capturePayment(String orderId) {
+        OrderCaptureResponse orderCaptureResponse = payPalService.captureOrder(orderId);
+        boolean completed = orderCaptureResponse.getStatus().equals("COMPLETED");
+
+        PaymentCaptureResponse paypalCaptureResponse = new PaymentCaptureResponse();
+        paypalCaptureResponse.setCompleted(completed);
+
+        if (completed) {
+            String purchaseIdStr = orderCaptureResponse.getPurchaseUnits().get(0).getReferenceId();
+            PurchaseDto purchaseDTO = purchaseService.confirmPurchase(Integer.parseInt(purchaseIdStr));
+            paypalCaptureResponse.setPurchaseId(purchaseDTO.getId());
+        }
+
+        return paypalCaptureResponse;
+    }
+}
